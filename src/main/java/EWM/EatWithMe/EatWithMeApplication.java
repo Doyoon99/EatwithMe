27d9efa7f6182225eff865e6a8b6a580cd9ee069
이름 <>
2022-05-08 21:57:47 +0900

    first

---
+++ src/main/java/EWM/EatWithMe/EatWithMeApplication.java
@@ -0,0 +1,13 @@
+package EWM.EatWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatWithMeApplication.class, args);
+	}
+
+}
