f2a8324ed83bc5e4d9ff1ba4711368b3fa2e7020
yunseo00 <chldbstj00@sookmyung.ac.kr>
2022-05-27 05:23:16 +0900

    store table db 연결

---
+++ src/main/java/EWM/EatWithMe/StoreController.java
@@ -0,0 +1,30 @@
+package EWM.EatWithMe;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/store")
+public class StoreController {
+    @Autowired
+    private StoreMapper storeMapper;
+
+    @PostMapping("")
+    public Storedata post(@RequestBody Storedata store) {
+        storeMapper.insert(store);
+
+        return store;
+    }
+
+    @GetMapping("")
+    public List<Storedata> getAll(){
+        return storeMapper.getAll();
+    }
+
+    @GetMapping("/{store_id}")
+    public Storedata getById(@PathVariable("store_id") int store_id){
+        return storeMapper.getById(store_id);
+    }
+}
---
+++ src/main/java/EWM/EatWithMe/Storedata.java
@@ -0,0 +1,15 @@
+package EWM.EatWithMe;
+
+
+import lombok.Data;
+
+@Data
+public class Storedata {
+
+    private int id;
+    private String name;
+    private String category;
+    private String address;
+    private String number;
+
+}
