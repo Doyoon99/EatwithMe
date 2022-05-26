480d757306e93c2d97b4eeba29efdf6d9b9c19c7
yunseo00 <chldbstj00@sookmyung.ac.kr>
2022-05-27 05:42:48 +0900

    prefer table db연결

---
+++ src/main/java/EWM/EatWithMe/PreferMapper.java
@@ -0,0 +1,24 @@
+package EWM.EatWithMe;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface PreferMapper {
+    @Insert("INSERT INTO prefer(user_id, store_id, rating) VALUES(#{prefer.user_id}, #{prefer.store_id}, #{prefer.rating})")
+    int insert(@Param("prefer") Preferdata prefer);
+
+    @Select("SELECT * FROM prefer")
+
+    @Results(id = "PreferMap", value = {
+            @Result(property = "user_id", column = "user_id"),
+            @Result(property = "store_id", column = "store_id"),
+            @Result(property = "rating", column = "rating")
+    })
+    List<Preferdata> getAll();
+
+    @Select("SELECT * FROM prefer WHERE user_id=#{user_id}")
+    @ResultMap("PreferMap")
+    Preferdata getById(@Param("user_id") int user_id);
+}
